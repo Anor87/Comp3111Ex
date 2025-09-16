7aa3f45c44e48ec4526da3ac163d57b93860541e
Anor87 <tntsang@connect.ust.hk>
2025-09-16 14:25:59 +0800

    Add a print line for fun

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -8,6 +8,7 @@ public class mainApp1 {
         int b = 2;
         int n = 11;
         System.out.println("Program ..Starting...");
+        System.out.println("haha ..... ");
         System.out.println("1 + 1 = "+2);
         System.out.println(b + " to power " + n + " = " + myLibrary.Power(b, n));
         System.out.println(n + "! = " + myLibrary.factorial(n));
